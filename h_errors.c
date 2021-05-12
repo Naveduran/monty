87c7bd5c5fcfa968e39b478432b6ba55ba83a056
Naveduran <naveduran@gmail.com>
2021-05-12 21:50:55 +0000

    a function to manage errors

---
+++ h_errors.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * error_manage - print to the stderr and stout
+ * @error: number of the error
+ */
+void error_manage(int error)
+{
+	if (error == 0)
+		printf("USAGE: monty file\n");
+	if (error == 1)
+		printf("Error: Can't open file %s\n", data->file_name);
+	if (error == 2)
+		printf("L%d: unknown instruction %s\n", data->line, opcode);
+	if (error == 3)
+		printf("Error: malloc failed\n");
+
+	fprintf(stderr, EXIT_FAILURE, strlen(EXIT_FAILURE));
+
+	return (0);
+}
