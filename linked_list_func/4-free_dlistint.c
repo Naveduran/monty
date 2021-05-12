39bea411fc2984c65a6a75bf7791fb37d6ea023a
lemejiamo <luismejia69@gmail.com>
2021-05-12 10:40:11 -0500

    upload linked list functions

---
+++ linked_list_func/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *prev = NULL;
+
+	if (!head)
+		return;
+
+	do {
+		prev = head->next;
+		free(head);
+		head = prev;
+	} while (head->next != NULL);
+
+	free(head);
+}
