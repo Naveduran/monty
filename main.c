702729453724a187f8c4a5d5f681ccf6361af925
lemejiamo <luismejia69@gmail.com>
2021-05-12 16:50:47 -0500

    add open sentence

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 int main(int argc, char *argv[])
 {
 	data_of_program data_struct = {NULL}, *data = &data_struct;
-	inicialize_data(data, argc, argv);
 
+	inicialize_data(data, argc, argv);
+	open_file(data->file_path);
 	error_code = string_len = read_file(data);
 
 	return (0);
