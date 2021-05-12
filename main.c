53f2111b604f7baaa726f2637809423c5186a676
lemejiamo <luismejia69@gmail.com>
2021-05-12 18:28:43 -0500

    main update flow

--- main.c
+++ main.c
@@ -10,10 +10,19 @@
 int main(int argc, char *argv[])
 {
 	data_of_program data_struct = {NULL}, *data = &data_struct;
+	char *line_tokens, *instr_tokens **gen_tokens;
 
 	inicialize_data(data, argc, argv);
-	open_file(data->file_path);
-	error_code = string_len = read_file(data);
+	/* open and read the data from file return a buffer to file data */
+	data->file_content = open_file(data->file_path);
+	/* tokenized of file data */
+
+	gen_tokens = _strtok(data->file_content, '\n');
+	for (; gen_tokens[i] != NULL, i++)
+	{
+		fun_tokens = _strtok(gen_tokens[i], ' ');
+		opcode_list(*fun_tokens[0], *fun_tokens[1])
+	}
 
 	return (0);
 }
