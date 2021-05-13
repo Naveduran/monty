b66f91b6aae225d80ed9d05228d66e4410f4cb58
Naveduran <naveduran@gmail.com>
2021-05-13 00:03:44 +0000

    basic structure of functions

---
+++ opcodes.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+/**
+ * push - pushes an element to the stack
+ * @data: data of program
+ */
+void push(data_of_program *data)
+{
+
+recibe un entero y lo mete en el stack
+
+	if (no recibe un entero)
+	{
+		error(data, 4);
+	}
+
+usar la funcion atoi
+
+}
+
+/**
+ * pall - prints all the values on the stack, starting from the top of the stack
+ * @data: data of program
+ */
+void pall(data_of_program *data)
+{
+	If the stack is empty, dont print anything
+}
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line
+ * @data: data of program
+ */
+void pint(data_of_program *data)
+{
+	if(!stack) stack esta vacio
+							 error(data, 5);
+}
+
+/**
+ * pop - removes the top element of the stack
+ * @data: data of program
+ */
+void pop(data_of_program *data)
+{
+	if(!stack) stack esta vacio
+							 error(data, 6);
+}
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @data: data of program
+ */
+void add(data_of_program *data)
+{
+
+	if(!stack) stack contiene menos de dos elementos
+							 error(data, 7);
+}
+
+/**
+ * add - adds the top two elements of the stack
+ * @data: data of program
+ */
+void add(data_of_program *data)
+{
+If the stack contains less than two elements
+	error(data, 8);
+}
+
+/**
+ * nop - doesn't do anything
+ * @data: data of program
+ */
+void nop(data_of_program *data)
+{
+}
