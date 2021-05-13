67a6de62c79db367cf3ce7721eea9aa37a6b9f2f
Naveduran <naveduran@gmail.com>
2021-05-13 00:16:12 +0000

    remember to free

--- h_errors.c
+++ h_errors.c
@@ -3,8 +3,9 @@
  * error_manage - print error message to stout and exit_failure to stderr
  * @error: number of the error
  */
-void error_manage(int error)
+void error_manage(data_of_program *data, int error)
 {
+
 	if (error == 0)
 		printf("USAGE: monty file\n");
 	if (error == 1)
@@ -13,5 +14,17 @@ void error_manage(int error)
 		printf("L%d: unknown instruction %s\n", data->line, opcode);
 	if (error == 3)
 		printf("Error: malloc failed\n");
+	if (error == 4)
+		printf("L%d: usage: push integer\n", data->line_number);
+	if (error == 5)
+		printf("L%d: can't pint, stack empty\n", data->line_number);
+	if (error == 6)
+		printf("L%d: can't pop an empty stack\n", data->line_number);
+	if (error == 7)
+		printf("L%d: can't swap, stack too short\n", data->line_number);
+	if (error == 8)
+		printf("L%d: can't add, stack too short\n", data->line_number);
+/* Liberar todo antes de salir */
 	exit(EXIT_FAILURE);
+
 }
