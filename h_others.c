12ba7987941da1ab83592367848a3aafb3f8b3ca
Naveduran <naveduran@gmail.com>
2021-05-13 10:29:58 +0000

    own str_compare

---
+++ h_others.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+/**
+ * str_compare - Compare two strings
+ * @string1: String one, or the shorter
+ * @string2: String two, or the longer
+ * @number: number of characters to be compared, 0 if infinite
+ * Return: 1 if the strings are equals,0 if the strings are different
+ */
+int str_compare(char *string1, char *string2, int number)
+{
+	int iterator;
+
+	if (string1 == NULL && string2 == NULL)
+		return (1);
+
+	if (string1 == NULL || string2 == NULL)
+		return (0);
+
+	if (number == 0) /* infinite longitud */
+	{
+		if (strlen(string1) != strlen(string2))
+			return (0);
+		for (iterator = 0; string1[iterator]; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+				return (0);
+		}
+		return (1);
+	}
+	else /* if there is a number of chars to be compared */
+	{
+		for (iterator = 0; iterator < number ; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+				return (0);
+		}
+		return (1);
+	}
+}
