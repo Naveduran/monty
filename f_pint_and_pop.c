0f5689100395bbd1b5d7abae381073883a7510ad
Naveduran <naveduran@gmail.com>
2021-05-13 16:23:56 +0000

    add functions

--- f_pint_and_pop.c
+++ f_pint_and_pop.c
@@ -3,20 +3,20 @@
  * pint - prints the value at the top of the stack, followed by a new line
  * @data: data of program
  */
-void pint(data_of_program *data UNUSED)
+void pint(data_of_program *data)
 {
 	if (!data->stack || (*data)->stack)
 		error(data, 5);
-	printf("\n", data->stack->n);
+	printf("%s\n", data->stack->n);
 }
 
 /**
  * pop - removes the top element of the stack
  * @data: data of program
  */
-void pop(data_of_program *data UNUSED)
+void pop(data_of_program *data)
 {
 	if (!data->stack)
 		error(data, 6);
-	/*...*/
+	delete_at_index(data->stack, 0);/* Erase the first node */
 }
