380a610a42e1f8db1f598d7d4dada77a0b64ab39
Naveduran <naveduran@gmail.com>
2021-05-14 03:46:31 +0000

    betty style

--- h_list2.c
+++ h_list2.c
@@ -1,15 +1,15 @@
 #include "monty.h"
 /**
- * delete_at_index - delete node at index position
+ * delete_at_end - delete node at index position
  * @stack: pointer to the head of stack
- * @index: index
  * Return: 1 if succes -1 if failed
  */
 void delete_at_end(stack_p *stack)
 {
 	stack_p *pop = NULL;
+
 	pop = stack;
-	if(pop->next == NULL )
+	if (pop->next == NULL)
 	{
 		pop->prev->next = NULL;
 		free(pop);
