5df7b48ca5f5c4eb551c06644129e42e662c83cc
lemejiamo <luismejia69@gmail.com>
2021-05-13 20:33:41 -0500

    implemented add function

--- f_swap_and_add.c
+++ f_swap_and_add.c
@@ -1,26 +1,42 @@
 #include "monty.h"
 /**
- * swap - swaps the top two elements of the stack
- * @data: data of program
+ * swap - swaps the top two elements of the stack.
+ * @data: pointer to the struc of info
+ * Return: none
  */
-void swap(data_of_program *data UNUSED)
+void swap(data_of_program *data)
 {
-	int sl = stack_len(data->stack);
+        stack_p *current = NULL;
+				int swap, i = 0;
 
-	if (sl < 2)
-		error(data, 7);
-	/*...*/
+        current = data->head;
+        for( ;current->next != NULL; current = current->next, i++)
+        {}
+				if (i < 2)
+					error(data, 7);
+
+				swap = current->n;
+        current->n = current->prev->n;
+        current->prev->n = swap;
 }
-
 /**
  * add - adds the top two elements of the stack
  * @data: data of program
  */
-void add(data_of_program *data UNUSED)
+void add(data_of_program *data)
 {
-	int sl = stack_len(data->stack);
+	int add = 0, i = 0;
+	stack_p *current = NULL;
 
-	if (sl < 2)
+	current = data->head;
+	for( ;current->next != NULL; current = current->next, i++)
+	{}
+	if (i < 2)
 		error(data, 8);
-	/*...*/
+
+	add = current->n + current->prev->n;
+	current->prev->next = NULL;
+	current->prev->n = add;
+	free(current);
+
 }
