d6ccc5b51bae062b364f08db9228b01bdecd9938
Naveduran <naveduran@gmail.com>
2021-05-14 04:21:09 +0000

    fprintf

--- h_errors.c
+++ h_errors.c
@@ -8,25 +8,26 @@ void error(data_of_program *data, int error)
 {
 	if (error == 0)
 	{
-		perror("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	if (error == 1)
-		perror("Error: Can't open file %s\n", data->file_path);
+		fprintf(stderr, "Error: Can't open file %s\n", data->file_path);
 	if (error == 2)
-		perror("L%d: unknown instruction %s\n", data->line_number, data->words[0]);
+		fprintf(stderr, "L%d: unknown instruction %s\n", data->line_number,
+						data->words[0]);
 	if (error == 3)
-		perror("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 	if (error == 4)
-		perror("L%d: usage: push integer\n", data->line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", data->line_number);
 	if (error == 5)
-		perror("L%d: can't pint, stack empty\n", data->line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", data->line_number);
 	if (error == 6)
-		perror("L%d: can't pop an empty stack\n", data->line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", data->line_number);
 	if (error == 7)
-		perror("L%d: can't swap, stack too short\n", data->line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", data->line_number);
 	if (error == 8)
-		perror("L%d: can't add, stack too short\n", data->line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", data->line_number);
 	free_all_data(data);
 	exit(EXIT_FAILURE);
 }
