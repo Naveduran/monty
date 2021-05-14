266be853ee6d66df45a9e8cb7db33d38ee43ac30
Naveduran <naveduran@gmail.com>
2021-05-14 05:06:52 +0000

    valgrind fix

--- h_free_memory.c
+++ h_free_memory.c
@@ -10,26 +10,15 @@ void free_array_of_pointers(char **array)
 {
 	int i;
 
-	if (array != NULL)
+	if (*array != NULL)
 	{
 		for (i = 0; array[i]; i++)
+		{
 			free(array[i]);
-
+		}
 		free(array);
 		array = NULL;
 	}
-}
-
-/**
- * free_recurrent_data - free the fields needed each readed line
- * @data: struct of the program's data
- * Return: Nothing
- */
-void free_recurrent_data(data_of_program *data)
-{
-/* Free double pointers */
-	if (data->words)
-		free_array_of_pointers(data->words);
 }
 
 /**
@@ -39,15 +28,12 @@ void free_recurrent_data(data_of_program *data)
  */
 void free_all_data(data_of_program *data)
 {
-	free_recurrent_data(data);
-	if (data->file_content)
-		free(data->file_content);
-	if (data->file_path)
-		free(data->file_path);
 	if (data->lines)
 		free_array_of_pointers(data->lines);
 	if (data->head)
 		free(data->head);
 	if (data->stack)
 		free_stack(data->stack);
+	if (data->words)
+		free_array_of_pointers(data->words);
 }
