918031337c6ba2cfad376fdb19227b6ee577b405
lemejiamo <luismejia69@gmail.com>
2021-05-16 22:30:00 -0500

    fixed minor bugs

--- h_errors.c
+++ h_errors.c
@@ -12,14 +12,17 @@ void error(data_of_program *data, int error)
 		exit(EXIT_FAILURE);
 	}
 	if (error == 1)
+	{
 		fprintf(stderr, "Error: Can't open file %s\n", data->file_path);
+		exit(EXIT_FAILURE);
+	}
 	if (error == 2)
 		fprintf(stderr, "L%d: unknown instruction %s\n", data->line_number,
 						data->words[0]);
 	if (error == 3)
 		fprintf(stderr, "Error: malloc failed\n");
 	if (error == 4)
-		fprintf(stderr, "L%d: usage: push integer\n", data->line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", (data->line_number+1));
 	if (error == 5)
 		fprintf(stderr, "L%d: can't pint, stack empty\n", data->line_number);
 	if (error == 6)
